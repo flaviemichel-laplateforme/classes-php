9dfae83535b87f27b5b853f34e8bbc19a16c9b64
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-11-13 13:48:21 +0100

    Ajout de la classe User avec des propriétés pour l'identifiant, le login, l'email, le prénom et le nom de famille.

---
+++ User.php
@@ -0,0 +1,18 @@
+<?php
+
+class User
+{
+    private $id;
+    public $login;
+    public $email;
+    public $firstname;
+    public $lastname;
+
+    public function __construct($id, $login, $email, $firstname, $lastname)
+    {
+        $this->login = $login;
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+}
